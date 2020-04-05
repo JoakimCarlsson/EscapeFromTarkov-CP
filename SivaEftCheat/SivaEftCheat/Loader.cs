4146ae4618ebc4d341966f905f30b34b6826f9c3
Carlsson <Carlsson@DESKTOP-14GSI35>
2020-04-05 14:10:33 +0100

    Added some basic classes and folders for structuring-

---
+++ SivaEftCheat/SivaEftCheat/Loader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SivaEftCheat
+{
+    public class Loader
+    {
+        public static GameObject HookObject;
+        public static void Load()
+        {
+            HookObject = new GameObject();
+            HookObject.AddComponent<Main>();
+            Object.DontDestroyOnLoad(HookObject);
+        }
+    }
+}
