046014192f4f2540087bbf8c2738e498f08136c1
Carlsson <Carlsson@DESKTOP-55HI4LQ>
2020-04-07 09:57:16 +0200

    Added a class for colours so we have all colours in one place. That way if we want the user too have the option it'll be easier too implement.

--- SivaEftCheat/SivaEftCheat/Utils/Colors.cs
+++ SivaEftCheat/SivaEftCheat/Utils/Colors.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace SivaEftCheat.Utils
 {
-    class Colors
+    internal static class Colors
     {
+        internal static Color extractColor = Color.green;
     }
 }
