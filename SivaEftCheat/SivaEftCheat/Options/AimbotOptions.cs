160e49dff44b4a9a08aaeeaeb048270fd905cd42
Carlsson <Carlsson@DESKTOP-55HI4LQ>
2020-04-12 06:44:08 +0200

    Refactored the Render class.

--- SivaEftCheat/SivaEftCheat/Options/AimbotOptions.cs
+++ SivaEftCheat/SivaEftCheat/Options/AimbotOptions.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace SivaEftCheat.Options
 {
     class AimbotOptions
     {
+        public static bool Aimbot = true;
+        public static KeyCode AimbotKey = KeyCode.LeftControl;
+        public static float Fov = 50f;
+        public static bool DrawFov = true;
     }
 }
--- SivaEftCheat/SivaEftCheat/Utils/Render.cs
+++ SivaEftCheat/SivaEftCheat/Utils/Render.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace SivaEftCheat.Utils
 {
     class Render
     {
-        public static Texture2D _currentTexture = null;
-        public static Color _currentTextureColor = Color.black;
-        private static Texture2D _texture;
-        private static Texture2D _boxTexture;
+        private static Color _texturesColor;
 
-        private static readonly GUIStyle _espLabelStyle = new GUIStyle
+        private static Texture2D _texture = new Texture2D(1, 1)
+        {
+            filterMode = 0
+        };
+
+        public static Texture2D CurrentTexture;
+        public static Color CurrentTextureColor = Color.black;
+        private static readonly GUIStyle EspLabelStyle = new GUIStyle
         {
             fontSize = 11,
         };
 
-        //FIX ME
         public static void DrawTextOutline(Vector2 position, string text, Color outColor, Color inColor)
         {
-            GUIStyle espLabelStyle = _espLabelStyle;
+            GUIStyle espLabelStyle = EspLabelStyle;
             Vector2 vector = espLabelStyle.CalcSize(new GUIContent(text));
             var rect = new Rect(position.x + 1f, position.y + 1f, vector.x + 12f, vector.y + 12f);
             espLabelStyle.normal.textColor = outColor;
@@ -34,98 +32,87 @@ namespace SivaEftCheat.Utils
         }
         public static void BoxRect(Rect rect, Color color)
         {
-                if (_currentTexture == null)
-                {
-                    _currentTexture = new Texture2D(1, 1);
-                    _currentTexture.SetPixel(0, 0, color);
-                    _currentTexture.Apply();
-                    _currentTextureColor = color;
-                }
-                else if (color != _currentTextureColor)
-                {
-                    _currentTexture.SetPixel(0, 0, color);
-                    _currentTexture.Apply();
-                    _currentTextureColor = color;
-                }
-                GUI.DrawTexture(rect, _currentTexture);
+            if (CurrentTexture == null)
+            {
+                CurrentTexture = new Texture2D(1, 1);
+                CurrentTexture.SetPixel(0, 0, color);
+                CurrentTexture.Apply();
+                CurrentTextureColor = color;
+            }
+            else if (color != CurrentTextureColor)
+            {
+                CurrentTexture.SetPixel(0, 0, color);
+                CurrentTexture.Apply();
+                CurrentTextureColor = color;
+            }
+            GUI.DrawTexture(rect, CurrentTexture);
         }
         public static void RectFilled(float x, float y, float width, float height, Texture2D texture)
         {
-                GUI.DrawTexture(new Rect(x, y, width, height), texture);
+            GUI.DrawTexture(new Rect(x, y, width, height), texture);
         }
 
         public static void DrawLine(Vector2 pointA, Vector2 pointB, float width, Color color)
         {
-                DrawLineStretched(pointA, pointB, (int)width, color);
+            DrawLineStretched(pointA, pointB, (int)width, color);
         }
 
         public static void DrawLineStretched(Vector2 lineStart, Vector2 lineEnd, int thickness, Color color)
         {
-                if (!_texture)
-                {
-                    _texture = new Texture2D(1, 1)
-                    {
-                        filterMode = 0
-                    };
-                }
-                Matrix4x4 matrix = GUI.matrix;
-                Color color2 = GUI.color;
-                GUI.color = color;
-                Vector2 vector = lineEnd - lineStart;
-                float num = (float)(57.29577951308232 * (double)Mathf.Atan(vector.y / vector.x));
-                if (vector.x < 0f)
-                {
-                    num += 180f;
-                }
-                if (thickness < 1)
-                {
-                    thickness = 1;
-                }
-                int num2 = (int)Mathf.Ceil((float)(thickness / 2));
-                GUIUtility.RotateAroundPivot(num, lineStart);
-                GUI.DrawTexture(new Rect(lineStart.x, lineStart.y - (float)num2, vector.magnitude, (float)thickness), _texture);
-                GUIUtility.RotateAroundPivot(-num, lineStart);
-                GUI.color = color2;
-                GUI.matrix = matrix;
+            Matrix4x4 matrix = GUI.matrix;
+            Color color2 = GUI.color;
+            GUI.color = color;
+            Vector2 vector = lineEnd - lineStart;
+            float num = (float)(57.29577951308232 * Mathf.Atan(vector.y / vector.x));
+            if (vector.x < 0f)
+            {
+                num += 180f;
+            }
+            if (thickness < 1)
+            {
+                thickness = 1;
+            }
+            int num2 = (int)Mathf.Ceil(thickness / 2);
+            GUIUtility.RotateAroundPivot(num, lineStart);
+            GUI.DrawTexture(new Rect(lineStart.x, lineStart.y - num2, vector.magnitude, thickness), _texture);
+            GUIUtility.RotateAroundPivot(-num, lineStart);
+            GUI.color = color2;
+            GUI.matrix = matrix;
         }
 
         public static void RectOutlined(float x, float y, float width, float height, Texture2D text, float thickness = 1f)
         {
-                RectFilled(x, y, thickness, height, text);
-                RectFilled(x + width - thickness, y, thickness, height, text);
-                RectFilled(x + thickness, y, width - thickness * 2f, thickness, text);
-                RectFilled(x + thickness, y + height - thickness, width - thickness * 2f, thickness, text);
+            RectFilled(x, y, thickness, height, text);
+            RectFilled(x + width - thickness, y, thickness, height, text);
+            RectFilled(x + thickness, y, width - thickness * 2f, thickness, text);
+            RectFilled(x + thickness, y + height - thickness, width - thickness * 2f, thickness, text);
         }
 
         public static void DrawBox(float x, float y, float width, float height, Color color, float thickness = 1f)
         {
-            if (!_boxTexture)
-            {
-                _boxTexture = new Texture2D(2, 2, TextureFormat.ARGB32, false);
-            }
-            _boxTexture.SetPixel(0, 0, color);
-            _boxTexture.SetPixel(1, 0, color);
-            _boxTexture.SetPixel(0, 1, color);
-            _boxTexture.SetPixel(1, 1, color);
-            _boxTexture.Apply();
+            _texture.SetPixel(0, 0, color);
+            _texture.SetPixel(1, 0, color);
+            _texture.SetPixel(0, 1, color);
+            _texture.SetPixel(1, 1, color);
+            _texture.Apply();
 
-            RectOutlined(x - width / 2f, y - height, width, height, _boxTexture, thickness);
+            RectOutlined(x - width / 2f, y - height, width, height, _texture, thickness);
         }
         public static void DrawHealth(Vector2 pos, float width, float height, float health, float maxHealth)
         {
-                BoxRect(new Rect(pos.x, pos.y, width, height), Color.black);
-                pos.x += 1f;
-                pos.y += 1f;
-                Color color = Color.green;
-                if (health <= 50f)
-                {
-                    color = Color.yellow;
-                }
-                if (health <= 25f)
-                {
-                    color = Color.red;
-                }
-                BoxRect(new Rect(pos.x, pos.y, width * (health / maxHealth * 100f) / 100f - 2f, height - 2f), color);
+            BoxRect(new Rect(pos.x, pos.y, width, height), Color.black);
+            pos.x += 1f;
+            pos.y += 1f;
+            Color color = Color.green;
+            if (health <= 50f)
+            {
+                color = Color.yellow;
+            }
+            if (health <= 25f)
+            {
+                color = Color.red;
+            }
+            BoxRect(new Rect(pos.x, pos.y, width * (health / maxHealth * 100f) / 100f - 2f, height - 2f), color);
         }
 
         public static void CornerBox(Vector2 headPosition, float width, float height, Color color, bool outline)
@@ -135,37 +122,36 @@ namespace SivaEftCheat.Utils
 
             if (outline)
             {
-                RectFilled(headPosition.x - width / 2f - 1f, headPosition.y - 1f, (float)(num + 2), 3f, Color.black);
-                RectFilled(headPosition.x - width / 2f - 1f, headPosition.y - 1f, 3f, (float)(num2 + 2), Color.black);
-                RectFilled(headPosition.x + width / 2f - (float)num - 1f, headPosition.y - 1f, (float)(num + 2), 3f, Color.black);
-                RectFilled(headPosition.x + width / 2f - 1f, headPosition.y - 1f, 3f, (float)(num2 + 2), Color.black);
-                RectFilled(headPosition.x - width / 2f - 1f, headPosition.y + height - 4f, (float)(num + 2), 3f, Color.black);
-                RectFilled(headPosition.x - width / 2f - 1f, headPosition.y + height - (float)num2 - 4f, 3f, (float)(num2 + 2), Color.black);
-                RectFilled(headPosition.x + width / 2f - (float)num - 1f, headPosition.y + height - 4f, (float)(num + 2), 3f, Color.black);
-                RectFilled(headPosition.x + width / 2f - 1f, headPosition.y + height - (float)num2 - 4f, 3f, (float)(num2 + 3), Color.black);
+                RectFilled(headPosition.x - width / 2f - 1f, headPosition.y - 1f, num + 2, 3f, Color.black);
+                RectFilled(headPosition.x - width / 2f - 1f, headPosition.y - 1f, 3f, num2 + 2, Color.black);
+                RectFilled(headPosition.x + width / 2f - num - 1f, headPosition.y - 1f, num + 2, 3f, Color.black);
+                RectFilled(headPosition.x + width / 2f - 1f, headPosition.y - 1f, 3f, num2 + 2, Color.black);
+                RectFilled(headPosition.x - width / 2f - 1f, headPosition.y + height - 4f, num + 2, 3f, Color.black);
+                RectFilled(headPosition.x - width / 2f - 1f, headPosition.y + height - num2 - 4f, 3f, num2 + 2, Color.black);
+                RectFilled(headPosition.x + width / 2f - num - 1f, headPosition.y + height - 4f, num + 2, 3f, Color.black);
+                RectFilled(headPosition.x + width / 2f - 1f, headPosition.y + height - num2 - 4f, 3f, num2 + 3, Color.black);
             }
-            RectFilled(headPosition.x - width / 2f, headPosition.y, (float)num, 1f, color);
-            RectFilled(headPosition.x - width / 2f, headPosition.y, 1f, (float)num2, color);
-            RectFilled(headPosition.x + width / 2f - (float)num, headPosition.y, (float)num, 1f, color);
-            RectFilled(headPosition.x + width / 2f, headPosition.y, 1f, (float)num2, color);
-            RectFilled(headPosition.x - width / 2f, headPosition.y + height - 3f, (float)num, 1f, color);
-            RectFilled(headPosition.x - width / 2f, headPosition.y + height - (float)num2 - 3f, 1f, (float)num2, color);
-            RectFilled(headPosition.x + width / 2f - (float)num, headPosition.y + height - 3f, (float)num, 1f, color);
-            RectFilled(headPosition.x + width / 2f, headPosition.y + height - (float)num2 - 3f, 1f, (float)(num2 + 1), color);
+            RectFilled(headPosition.x - width / 2f, headPosition.y, num, 1f, color);
+            RectFilled(headPosition.x - width / 2f, headPosition.y, 1f, num2, color);
+            RectFilled(headPosition.x + width / 2f - num, headPosition.y, num, 1f, color);
+            RectFilled(headPosition.x + width / 2f, headPosition.y, 1f, num2, color);
+            RectFilled(headPosition.x - width / 2f, headPosition.y + height - 3f, num, 1f, color);
+            RectFilled(headPosition.x - width / 2f, headPosition.y + height - num2 - 3f, 1f, num2, color);
+            RectFilled(headPosition.x + width / 2f - num, headPosition.y + height - 3f, num, 1f, color);
+            RectFilled(headPosition.x + width / 2f, headPosition.y + height - num2 - 3f, 1f, num2 + 1, color);
         }
 
-        private static Color textureColor;
-        private static Texture2D texture = new Texture2D(1, 1);
+
 
         public static void RectFilled(float x, float y, float width, float height, Color color)
         {
-            if (color != textureColor)
+            if (color != _texturesColor)
             {
-                textureColor = color;
-                texture.SetPixel(0, 0, color);
-                texture.Apply();
+                _texturesColor = color;
+                _texture.SetPixel(0, 0, color);
+                _texture.Apply();
             }
-            GUI.DrawTexture(new Rect(x, y, width, height), texture);
+            GUI.DrawTexture(new Rect(x, y, width, height), _texture);
         }
     }
 }
